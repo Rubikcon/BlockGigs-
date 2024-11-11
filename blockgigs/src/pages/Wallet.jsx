3f3d2307d02296d2ec7f791d9759c3b7622bf8ce
Moses <moseschizaram8@gmail.com>
2024-11-11 18:45:52 +0100

    Add logic to detect and connect metamask

--- blockgigs/src/pages/Wallet.jsx
+++ blockgigs/src/pages/Wallet.jsx
@@ -1,8 +1,13 @@
 // pages/Wallet.js
 import React from "react";
+const eth = "../config/detect"
 
 const Wallet = () => {
-  return <div><h1>Wallet Page</h1></div>;
+  return <div>
+    <h1>Wallet Page</h1> 
+    <button class="enableEthereumButton">Connect Ethereum</button>
+    <h2>Account: <span class="showAccount"></span></h2>
+   </div>;
 };
 
 export default Wallet;
